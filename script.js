6f12f1bf9210616f2d5cc6b69d9fac250510ddb4
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-04 04:01:00 -0500

    Adding alert js code

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 function init(){
-//add your javascrip between these two lines of code
+  function myEventFunction(){
+    alert('Hello')
+  }
+  
+  var d4 = document.getElementById('container')
+  
+  d4.addEventListener('click', myEventFunction);
+  
+  d4.addEventListener('click', function()){
+  		console.log('Clicked!');
+  		alert(container.value);
+  		container.value = 'javascript worked';
+  };
+  
+}
+
+window.addEventListener('load', init);
