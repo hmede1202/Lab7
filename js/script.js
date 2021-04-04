7e340b9dc25548e08af89fd670b4bb126da99cb2
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-04 09:51:29 -0500

    Added output to js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 function init(){
   function myEventFunction(){
-    alert('Hello')
+    alert('Hello!')
   }
   
   var d4 = document.getElementById('entrybutton')
+  
+  var d5 = document.getElementById('textoutput')
   
   d4.addEventListener('click', myEventFunction);
   
   d4.addEventListener('click', function(){
   		console.log('I was Clicked!');		
-  		alert(d4.value);
-  		d4.value = 'javascript worked';
+  		alert(d5.value);
+  		d5.value = 'Hunter Mede:';
   });
   
 }
