fdd19bd946c3152265d4d898a3c9105c00b79e29
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-04 11:08:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ function init(){
   }
   
   var d4 = document.getElementById('entrybutton')
+  var d5 = document.getElementById('entryinput')
+  var d6 = document.getElementById('textoutput')
   
   d4.addEventListener('click', myEventFunction);
   
   d4.addEventListener('click', function(){
-  		console.log('I was Clicked!');	
-   });
-   
-  var d5 = document.getElementById('entryinput')
-  
-   d5.addEventListener('click', function(){	
-  		alert(d5.value);
-  		d5.value = 'Hunter Mede:';
-  		d6.innerHTML = 'd5';
-  		});
+    console.log('I was Clicked!');	
+    });
+
+  d4.addEventListener('click', function(){	
+  	alert(d5.value);
+  	d5.value = 'Hunter Mede:';		
+  	d6.innerHTML = 'd5';
+  	});
 
 window.addEventListener('load', init);
