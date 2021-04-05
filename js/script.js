d75d473350235bbc3606f3f6bb78f11496fabca8
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-05 13:37:56 -0500

    Fixing to get output correct

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 function init(){
-  function myEventFunction(){
-    alert('Hello!');
-  }
+ 
   
   var d4 = document.getElementById('entrybutton');
   var d5 = document.getElementById('entryinput');
   var d6 = document.getElementById('textoutput');
 
-  d4.addEventListener('click', myEventFunction);
   
   d4.addEventListener('click', function(){
-    console.log('An Entry!!!'); 
-    d5.value = "Hunter Mede:";
-    alert(d5.value);
-    d6.value = d5;
-    d6.innerHTML = d5;  
+  alert("Hunter Mede:" + d5.value); 
+  d6.innerHTML="";
+  d6.innerHTML= d5.value;
   });
 
 }
