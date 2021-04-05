4d9806dc54953d89175df2d2747b7b46a7b1d2f8
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-05 00:16:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,16 @@ function init(){
   }
   
   var d4 = document.getElementById('entrybutton');
+  var d5 = document.getElementById('entryinput');
+  var d6 = document.getElementById('textoutput');
 
-  d4.addEventListener('click', myEventFunction);
+  d4.addEventListener('click', myEventFunction();
   
   d4.addEventListener('click', function(){
     console.log('An Entry!!!');	
-    alert(d4.value);
-    d4.value = "Hunter Mede:"
+    alert(d5.value);
+    d5.value = "Hunter Mede:";
+    d6.innerHTML = 'd5';  
   });
 
 window.addEventListener('load', init);
